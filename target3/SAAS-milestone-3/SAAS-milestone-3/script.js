c1ee1c7f23f886694ce30d79b7de190db96ea9f2
Aravind2799 <52705445+Aravind2799@users.noreply.github.com>
2019-10-22 11:58:33 +0530

    Add files via upload

---
+++ target3/SAAS-milestone-3/SAAS-milestone-3/script.js
@@ -0,0 +1,63 @@
+// This is a closure function https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-closure-b2f0d2152b36
+(function() {
+  var initialize = function() {
+    /*
+      1. Add all your event bindings here. Please avoid binding events inline and add your event listeners here.
+
+      onSubmit callback
+      disableDuplicateSecondaryDepartment callback,...
+    */
+  };
+
+  var disableDuplicateSecondaryDepartment() = function() {
+    // 2. in department2, Should disable the option selected in department1
+  }
+
+  var constructData() = function() {
+    var data = {};
+
+    // 3. Construct data from the form here. Please ensure that the keys are the names of input elements
+
+    return data;
+  }
+
+  var validateResults = function(data) {
+    var isValid = true;
+
+    // 4. Check if the data passes all the validations here
+
+    return isValid;
+  };
+
+  var onSubmit = function() {
+    var data = constructData();
+
+    if (validateResults(data)) {
+      printResults(data);
+    } else {
+      var resultsDiv = document.getElementById("results");
+      resultsDiv.innerHTML = '';
+      resultsDiv.classList.add("hide");
+    }
+  };
+
+  var printResults = function(data) {
+    var constructElement = function([key, value]) {
+      return `<p class='result-item'>${key}: ${value}</p>`;
+    };
+
+    var resultHtml = (Object.entries(data) || []).reduce(function(innerHtml, keyValuePair) {
+      debugger
+      return innerHtml + constructElement(keyValuePair);
+    }, '');
+    var resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = resultHtml;
+    resultsDiv.classList.remove("hide");
+  };
+
+  /*
+    Initialize the javascript functions only after the html DOM content has loaded.
+    This is to ensure that the elements are present in the DOM before binding any event listeners to them.
+  */
+  document.addEventListener('DOMContentLoaded', initialize);
+})();
